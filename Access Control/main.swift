6dcd156158b5bfc56682a00968a54cc356cefe29
Temich93s <nil>
2021-10-14 15:00:57 +1000

    Пройдена тема: Синтаксис контроля доступа

--- Access Control/main.swift
+++ Access Control/main.swift
@@ -9,3 +9,19 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: Синтаксис контроля доступа
+print("\n//Синтаксис контроля доступа")
+
+public class SomePublicClass {}
+internal class SomeInternalClass {}
+fileprivate class SomeFilePrivateClass {}
+private class SomePrivateClass {}
+ 
+public var somePublicVariable = 0
+internal let someInternalConstant = 0
+fileprivate func someFilePrivateFunction() {}
+private func somePrivateFunction() {}
+
+class SomeInternalClass1 {}              // неявно internal
+let someInternalConstant1 = 0            // неявно internal
+
